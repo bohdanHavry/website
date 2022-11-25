0b0cb2fab70979a3ba64d67cbe4390bc5643b004
artemhamotskyi <118213150+artemhamotskyi@users.noreply.github.com>
2022-11-25 20:28:29 +0200

    25.11.2022 20:25

---
+++ src/main/java/com/example/store/controller/MainController.java
@@ -0,0 +1,22 @@
+package com.example.store.controller;
+
+import org.springframework.stereotype.Controller;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
+        return "greeting";
+    }
+
+    @GetMapping("/main")
+    public String main(Map<String, Object> model){
+        model.put("some","Hello Store!");
+        return "main";
+    }
+
+}
